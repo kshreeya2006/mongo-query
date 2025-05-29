4ec10b6070ae8ea21afeab40390673bffc6ac6d4
kshreeya2006 <kshreeya2006@gmail.com>
2025-05-29 18:48:40 +0530

    Added products collection

---
+++ q1.js
@@ -0,0 +1,11 @@
+//cmd> type mongosh> get 'test' prompt
+//type cls to clear screen
+//use gcet: creates new database named 'gcet' if it does not already exist or open database gcet if it doesnt exist
+//show collection: shows collections(tables)
+//db.createCollection("<collection-name>")
+//
+//
+//
+//
+//
+
